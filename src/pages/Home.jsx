f03e103fbc6e7a6cab3b9d8573890ccb00e5495e
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2025-06-24 15:38:04 +0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = ()=> {
     return(
         <div style={{alignItems: 'center', margin: 0, display: 'flex', flexDirection: 'column', justifyContent: 'center', minWidth: '100vw', minHeight:'100vh', backgroundColor: 'white'}}>
             <Title style={{color: '#2f27ce', fontWeight: '700'}}>
-             Welcome to the Post Blog
+             Welcome to the KADA Blog
             </Title>
             <Title level={4}>Made using React</Title>
             <Title level={5}>By Calista Salsabila</Title>
