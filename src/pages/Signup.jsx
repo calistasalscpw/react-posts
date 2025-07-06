568b043463b39353d7373f5c52b686362f1f5b71
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2025-07-06 20:28:27 +0700

    Create Signup.jsx

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import { useState } from 'react';
+
+const Signup = () => {
+
+    return(
+        <Form
+      name="login"
+      initialValues={{ remember: true }}
+      style={{ maxWidth: 360 }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+      </Form.Item>
+      <Form.Item>
+        <Flex justify="space-between" align="center">
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+          <a href="">Forgot password</a>
+        </Flex>
+      </Form.Item>
+
+      <Form.Item>
+        <Button block type="primary" htmlType="submit">
+          Log in
+        </Button>
+        or <a href="">Register now!</a>
+      </Form.Item>
+    </Form>
+    )
+}
+
+export default Signup;
