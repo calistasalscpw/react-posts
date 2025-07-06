eee386ca3694848aaba2a14a938fd87894c67e53
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2025-07-06 19:31:56 +0700

    Update user.js

--- backend/routers/user.js
+++ backend/routers/user.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
 import User from '../models/users.model.js';
 import passport from '../config/passport.js';
 
