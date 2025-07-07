23c0a00b17bb335b7e1a4ab18f51e7d004a73cf2
Calista Candramawa <121235720+calistasalscpw@users.noreply.github.com>
2025-07-07 10:35:18 +0700

    Update post.validator.js

--- backend/validators/post.validator.js
+++ backend/validators/post.validator.js
@@ -1,6 +1,24 @@
+import Post from './../models/posts.model';
 export async function isUserValidation (req, res, next){
+
     const user = req.user;
     if (!user){
         res.json({ message: 'You are not authorized. Please login.' });
     } next ();
+}
+
+export async function isSameUserValidator(req, res, next){
+    const user = req.user;
+    
+    if (!user){
+        return res.json({ message: 'You are not authorized. Please login.' });
+    }
+
+    const post = await Post.findById(req.params.postId);
+
+    if(!post.author._id.equals(user._id)){
+        res.status(403).json('Not authorized to modify this post');
+    }
+    
+    next();
 }
